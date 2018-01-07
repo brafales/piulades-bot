9b5e2a8ff80a644f7dc8e0919768685a1365728d
Bernat Rafales <brafales@gmail.com>
2018-01-07 10:18:04 +0000

    Switch back to webhooks and disable polling

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"log"
-	"time"
-	//"net/http"
-	//"os"
-
 	"github.com/brafales/piulades-bot/configuration"
 	"github.com/brafales/piulades-bot/handler"
 	"github.com/brafales/piulades-bot/pinchito"
-	//"github.com/brafales/piulades-bot/twitter"
+	"github.com/brafales/piulades-bot/twitter"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -19,29 +17,17 @@ func main() {
 
 	bot, err := tgbotapi.NewBotAPI(config.BotKey)
 	fail(err)
-	/*
-		twitterClient := twitter.NewClient(config.TwitterAPIKey, config.TwitterAPISecret)
+	twitterClient := twitter.NewClient(config.TwitterAPIKey, config.TwitterAPISecret)
 
-		_, err = bot.SetWebhook(tgbotapi.NewWebhook(config.CallbackURL))
-		fail(err)
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(config.CallbackURL))
+	fail(err)
 
-		updates := bot.ListenForWebhook("/")
-		go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	updates := bot.ListenForWebhook("/")
+	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 
-		twitterHandler := &handler.Twitter{Bot: bot, ChatID: config.ChatID, TwitterClient: twitterClient}
-		tapetaHandler := &handler.Tapeta{Bot: bot, ChatID: config.ChatID}
-		searchHandler := &handler.Search{Bot: bot, ChatID: config.ChatID}
-		handlers := []handler.Handler{twitterHandler, tapetaHandler, searchHandler}
-
-		log.Println("Ready to handle messages")
-		for update := range updates {
-			for _, h := range handlers {
-				log.Println("handling")
-				h.Handle(update)
-			}
-		}
-	*/
-
+	twitterHandler := &handler.Twitter{Bot: bot, ChatID: config.ChatID, TwitterClient: twitterClient}
+	tapetaHandler := &handler.Tapeta{Bot: bot, ChatID: config.ChatID}
+	searchHandler := &handler.Search{Bot: bot, ChatID: config.ChatID}
 	pinchito.InitUsers()
 
 	pinchitoClient := pinchito.Client{
@@ -55,22 +41,15 @@ func main() {
 		AuthToken:      config.PinchitoAuthToken,
 		PinchitoClient: pinchitoClient,
 	}
+	handlers := []handler.Handler{twitterHandler, tapetaHandler, searchHandler, crearHandler}
 
-	conf := tgbotapi.NewUpdate(0)
-	for {
-		updates, _ := bot.GetUpdates(conf)
-		if len(updates) > 0 {
-			for _, update := range updates {
-				crearHandler.Handle(update)
-
-				if update.UpdateID >= conf.Offset {
-					conf.Offset = update.UpdateID + 1
-				}
-			}
+	log.Println("Ready to handle messages")
+	for update := range updates {
+		for _, h := range handlers {
+			log.Println("handling")
+			h.Handle(update)
 		}
-		time.Sleep(5 * time.Second)
 	}
-
 }
 
 func fail(err error) {
