3afd911db059fd89884da194d443b57e44df616f
Sergi Vélez <ris@risdevs.com>
2018-01-09 23:33:59 +0100

    Fixes 1.0 (#4)
    * Revert "Merge pull request #2 from brafales/revert-1-feature/save-log"
    
    This reverts commit 28610f4c5f0b885f85bde2cb430139048a0c0a3e, reversing
    changes made to 0d1d055253e4a2f41064ed46d174b3cb12662f65.
    
    * added n0seres
    
    * crear is ignoring any message that it's not private and any message that might interfere with other commands
    
    * Fixed some crashes

---
+++ pinchito/users.go
@@ -0,0 +1,55 @@
+package pinchito
+
+type TgPinchitoUser struct {
+	TgUsername string
+	PinId      int
+	PinNick    string
+}
+
+var tgPinchitoUsers []TgPinchitoUser
+
+func init() {
+	tgPinchitoUsers = []TgPinchitoUser{
+
+		{"PaRaP", 1, "PaRaP"},
+		{"rofirrim", 2, "rofi"},
+		{"SiR_RiS", 36, "[RiS]"},
+		{"frikjan", 4, "Freakhand"},
+		{"The_Bell", 18, "The_Bell"},
+		{"hexxx", 24, "hex"},
+		{"ciddx", 19, "Cid"},
+		{"javiKug", 30, "KuG"},
+		{"mixtura", 8, "mixtura"},
+		{"santiee", 13, "Santiee"},
+		{"hydex86", 5, "HyDe"},
+		{"n0seres", 41, "n0seres"},
+		{"", 3, "CHeRNoBiL"},
+		{"", 6, "KoLiFLoR"},
+		{"", 7, "Sharek"},
+		{"", 9, "^smith"},
+		{"", 10, "Menxu"},
+		{"", 11, "ReiVaX18"},
+		{"", 12, "yorx"},
+		{"", 14, "Enehy"},
+		{"", 15, "dikFIB"},
+		{"", 16, "gOBl1N"},
+		{"", 17, "Scenix"},
+		{"", 20, "{nimfa}"},
+		{"", 21, "Kayl"},
+		{"", 22, "graz"},
+		{"", 23, "CrC"},
+		{"", 25, "[TUmor]"},
+		{"", 26, "basted"},
+		{"", 27, "SLaKeLS"},
+		{"", 28, "AC_DC"},
+		{"", 29, "STeArN"},
+		{"", 31, "Yak-3"},
+		{"", 34, "NeoRob"},
+		{"", 32, "{xtreme}"},
+		{"", 33, "^KreatoR"},
+		{"", 35, "qiz"},
+		{"", 37, "{_SaLeM_"},
+		{"", 38, "FCS"},
+		{"", 39, "DeNeA"},
+	}
+}
